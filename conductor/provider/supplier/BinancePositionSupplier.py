8868b6078b45bde700fe3d14b30f6783735f46d6
Eugene the Red <me@eugenethered.com>
2022-05-25 10:40:37 +0100

    better exception handling

--- conductor/provider/supplier/BinancePositionSupplier.py
+++ conductor/provider/supplier/BinancePositionSupplier.py
@@ -1,10 +1,12 @@
 import logging
 from typing import List
 
+from binance.error import ClientError
 from binance.spot import Spot as Client
 from core.number.BigFloat import BigFloat
 from core.position.Position import Position
 from coreauth.AuthenticatedCredentials import AuthenticatedCredentials
+from coreauth.exception.UnableToAuthenticateError import UnableToAuthenticateError
 from coreutility.collection.dictionary_utility import as_data
 from position.provider.supplier.PositionSupplier import PositionSupplier
 
@@ -28,23 +30,28 @@ class BinancePositionSupplier(PositionSupplier):
             self.client = Client(self.api_key, self.api_secret)
 
     def get_positions(self) -> List[Position]:
-        self.lazy_init_client()
-        account_info = self.client.account()
-        position_balances = account_info['balances']
-        return self.extract_positions(position_balances)
+        try:
+            self.lazy_init_client()
+            account_info = self.client.account()
+            event_time = account_info['updateTime']
+            position_balances = account_info['balances']
+            return self.extract_positions(position_balances, event_time)
+        except ClientError as binance_client_error:
+            self.log.warning(f'Unable to authenticate binance client, error:[{binance_client_error.error_message}]')
+            raise UnableToAuthenticateError(binance_client_error.error_message)
 
-    def extract_positions(self, position_balances) -> List[Position]:
+    def extract_positions(self, position_balances, event_time) -> List[Position]:
         if position_balances is None or len(position_balances) == 0:
             return []
-        parsed_positions = [position for position_balance in position_balances if (position := self.parse_position(position_balance)) is not None]
+        parsed_positions = [position for position_balance in position_balances if (position := self.parse_position(position_balance, event_time)) is not None]
         return list(parsed_positions)
 
-    def parse_position(self, position_balance):
+    def parse_position(self, position_balance, event_time):
         instrument = as_data(position_balance, 'asset')
         available_balance = BigFloat(as_data(position_balance, 'free', '0.00'))
         unavailable_balance = BigFloat(as_data(position_balance, 'locked', '0.00'))
         if self.is_position_available(available_balance, unavailable_balance):
-            return Position(instrument=instrument, quantity=available_balance)
+            return Position(instrument=instrument, quantity=available_balance, instant=event_time)
 
     @staticmethod
     def is_position_available(available_balance, unavailable_balance):
--- simulation/positionsimulation/__init__.py
+++ simulation/positionsimulation/__init__.py
@@ -1,14 +1,22 @@
+from cache.holder.RedisCacheHolder import RedisCacheHolder
+
 from conductor.provider.supplier.BinancePositionSupplier import BinancePositionSupplier
 
 if __name__ == '__main__':
 
     options = {
-        'BINANCE_API_KEY': '<API-KEY>',
-        'BINANCE_API_SECRET': '<API-SECRET>'
+        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_PORT': 6379,
+        'POSITION_KEY': 'test:position',
+        'POSITION_HISTORY_LIMIT': 10,
+        'AUTH_INFO_KEY': 'binance:auth:info'
     }
+
+    RedisCacheHolder(options)
 
     position_supplier = BinancePositionSupplier(options)
     positions = position_supplier.get_positions()
+
     print('positions are:')
     print('-----------------------')
     for position in positions:
