3642ee0fc7a226b4d29b4e8034daac61a075389d
Eugene the Red <me@eugenethered.com>
2022-07-29 18:20:11 +0100

    bump version + deps

--- simulation/fetchdata/__init__.py
+++ simulation/fetchdata/__init__.py
@@ -2,25 +2,26 @@ import logging
 import time
 
 from cache.holder.RedisCacheHolder import RedisCacheHolder
+from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
 
 from conductor.BinancePositionConductor import BinancePositionConductor
 
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
         'POSITION_SLIP_KEY': 'test:position:slip',
         'POSITION_KEY': 'test:position',
         'POSITION_HISTORY_LIMIT': 10,
         'AUTH_INFO_KEY': 'binance:auth:info',
-        'PROCESS_KEY': '{}:process:status:{}',
-        'PROCESS_RUN_PROFILE_KEY': '{}:process:run-profile:{}'
+        'PROCESS_KEY': 'binance:process:mv:status',
+        'PROCESS_RUN_PROFILE_KEY': 'binance:process:mv:run-profile'
     }
 
     logging.basicConfig(level=logging.DEBUG)
 
-    RedisCacheHolder(options)
+    RedisCacheHolder(options, held_type=RedisCacheProviderWithHash)
 
     start_time = time.perf_counter()
 
--- simulation/runprofile/__init__.py
+++ simulation/runprofile/__init__.py
@@ -1,16 +1,17 @@
 from cache.holder.RedisCacheHolder import RedisCacheHolder
+from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
 from processrepo.ProcessRunProfile import ProcessRunProfile, RunProfile
 from processrepo.repository.ProcessRunProfileRepository import ProcessRunProfileRepository
 
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
-        'PROCESS_RUN_PROFILE_KEY': '{}:process:run-profile:{}'
+        'PROCESS_RUN_PROFILE_KEY': 'binance:process:mv:run-profile'
     }
 
-    RedisCacheHolder(options)
+    RedisCacheHolder(options, held_type=RedisCacheProviderWithHash)
 
     process_run_profile = ProcessRunProfile('binance', 'position-conductor', RunProfile.DAY, True)
 
