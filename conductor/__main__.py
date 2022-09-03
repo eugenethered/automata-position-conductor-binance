9236317abccd865bcd5868b649442bf82eebf738
Eugene The Red <me@eugenethered.com>
2022-09-03 19:20:53 +0100

    using env var feature

--- conductor/__main__.py
+++ conductor/__main__.py
@@ -2,9 +2,8 @@ import logging
 
 from cache.holder.RedisCacheHolder import RedisCacheHolder
 from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
-from core.arguments.command_line_arguments import option_arg_parser
+from core.environment.EnvironmentVariables import EnvironmentVariables
 from logger.ConfigureLogger import ConfigureLogger
-from metainfo.MetaInfo import MetaInfo
 
 from conductor.BinancePositionConductor import BinancePositionConductor
 
@@ -12,17 +11,14 @@ from conductor.BinancePositionConductor import BinancePositionConductor
 def start():
     ConfigureLogger()
 
-    meta_info = MetaInfo('persuader-technology-automata-position-conductor-binance')
-
-    command_line_arg_parser = option_arg_parser(meta_info)
-    args = command_line_arg_parser.parse_args()
+    environment_variables = EnvironmentVariables()
 
     log = logging.getLogger('Binance Position Conductor')
     log.info('position conductor initialized')
 
-    RedisCacheHolder(args.options, held_type=RedisCacheProviderWithHash)
+    RedisCacheHolder(environment_variables.options, held_type=RedisCacheProviderWithHash)
 
-    conductor = BinancePositionConductor(args.options)
+    conductor = BinancePositionConductor(environment_variables.options)
     conductor.start_process_schedule()
 
 
